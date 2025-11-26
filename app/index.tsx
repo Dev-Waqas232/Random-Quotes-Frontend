781a0b34e8b87ee62dd751e6a7ae4625a3f9ae69
Dev-Waqas232 <devwaqas232@gmail.com>
2025-11-26 21:41:49 +0500

    feat: integrated API to get random quote

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { useQuote } from "@/hooks/use-quote";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const { loading, quote } = useQuote();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.quote}>{quote?.quote}</Text>
+      <Text style={styles.author}>{quote?.author && `~ ${quote.author}`}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  quote: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 20,
+  },
+  author: {
+    fontStyle: "italic",
+  },
+});
---
+++ hooks/use-quote.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export function useQuote() {
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState<{
+    id: number;
+    quote: string;
+    author: string;
+  } | null>(null);
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  async function getQuote() {
+    try {
+      setLoading(true);
+      setQuote(null);
+
+      console.log(process.env.BACKEND_URL);
+      const response = await fetch(
+        "https://random-quotes-backend-t4ol.onrender.com/quote"
+      );
+      if (!response.ok) {
+        const err = new Error("API Request Failed");
+        throw err;
+      }
+
+      const data = (await response.json()) as {
+        ok: boolean;
+        data: { id: number; quote: string; author: string };
+      };
+
+      setQuote(data.data);
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { loading, quote };
+}
